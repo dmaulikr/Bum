1a7aae730eae0be7d84d12cfd150e6ab4e9ec3d1
Grant Davis <grant.davis@gmail.com>
2013-03-10 19:05:21 -0600

    move component stub

---
+++ Bum/Bum/MovementComponent.h
@@ -0,0 +1,17 @@
+//
+//  MovementComponent.h
+//  Bum
+//
+//  Created by Grant Davis on 3/10/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "Component.h"
+
+@interface MovementComponent : Component {
+    
+}
+
+@end
