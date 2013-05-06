dac3940b0b73f751ca01ed91b657b04537c43aee
Grant Davis <grant.davis@gmail.com>
2013-05-05 18:14:43 -0600

    adds flame object that when collided with does damage and knocks the player back

---
+++ BumUnity/Assets/Scripts/DamagePlayerOnTouch.js
@@ -0,0 +1,28 @@
+#pragma strict
+
+public var damageAmount : int = 10;
+public var bounceBackVelocity :float = 200.0;
+
+private var VELOCITY_DAMPING :float = 0.5;
+private var _player :GameObject;
+private var _health :Health;
+private var _playerController :PlayerController;
+
+function Start () {
+	_player = GameObject.Find("Player");
+	_health = _player.GetComponent(Health);
+	_playerController = _player.GetComponent(PlayerController);
+}
+
+function OnTriggerEnter( collider : Collider )
+{
+	if (collider.gameObject.name == "Player") {
+	
+		_health.Damage( damageAmount );
+		
+		// apply a reactive force to the player to knock them back		
+		var distance :Vector3 = _player.transform.position - this.gameObject.transform.position;
+		var normalizedDistance :Vector3 = distance.normalized;
+		_player.rigidbody.AddForce( Vector3(bounceBackVelocity * normalizedDistance.x, bounceBackVelocity * normalizedDistance.y, 0), ForceMode.VelocityChange );
+	}
+}
--- BumUnity/Assets/Scripts/Health.js
+++ BumUnity/Assets/Scripts/Health.js
@@ -8,15 +8,16 @@ public function isDead() :boolean
 	return current <= 0;
 }
 
-public function apply( amount : int )
+public function Heal( amount : int )
 {
-	current += amount;
+	current = Mathf.Min( current + amount, total);
+	this.gameObject.SendMessage("OnHealthChange", current, SendMessageOptions.DontRequireReceiver);
+	this.gameObject.SendMessage("OnHeal", current, SendMessageOptions.DontRequireReceiver);
 }
 
-function Start () {
-
-}
-
-function Update () {
-
+public function Damage( amount : int )
+{
+	current = Mathf.Max( current - amount, 0);
+	this.gameObject.SendMessage("OnHealthChange", current, SendMessageOptions.DontRequireReceiver);
+	this.gameObject.SendMessage("OnDamage", current, SendMessageOptions.DontRequireReceiver);
 }
